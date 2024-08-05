a01ba516fb2ce3faeedba736f101f1471a2fe319
Peal Hasan <pealh0320@gmail.com>
2024-08-04 22:46:47 -0400

    UPDATE

---
+++ BestTimeToBuySellStock.java
@@ -0,0 +1,19 @@
+public class BestTimeToBuySellStock {
+    public int maxProfit(int[] prices) {
+        int fastPointer = 1;
+        int slowPointer = 0;
+        int profit = 0;
+        int maxProfit = 0;
+
+        while(fastPointer < prices.length){
+            profit = prices[fastPointer] - prices[slowPointer];
+            maxProfit = Math.max(profit, maxProfit);
+            if(profit <= 0){
+                slowPointer = fastPointer;
+                fastPointer++;
+            } else fastPointer++;
+        }
+        if(maxProfit <= 0) maxProfit = 0;
+        return maxProfit;
+    }
+}
