dda0a9a22d996aff9b6daad7380ed65b1d09327e
Peal Hasan <pealh0320@gmail.com>
2024-08-06 21:32:59 -0400

    UPDATE

---
+++ LongestSubsWithoutRepChar.java
@@ -0,0 +1,25 @@
+import java.util.HashSet;
+
+public class LongestSubsWithoutRepChar {
+    public int lengthOfLongestSubstring(String s) {
+        if(s.length() == 0) return 0;
+        if(s.length() == 1) return 1;
+        
+        int left = 0;
+        int right = 0;
+        int max = 0;
+        HashSet <Character> set = new HashSet<>();
+
+        while(right < s.length()){
+            if(!set.contains(s.charAt(right))){
+                set.add(s.charAt(right));
+                max = Math.max(max, set.size());
+                right++;
+            } else {
+                set.remove(s.charAt(left));
+                left++;
+            }
+        }
+        return max;
+    }
+}
