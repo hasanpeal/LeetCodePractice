13aaddd61f453b8edd344750b71b47ea111f3675
Peal Hasan <95323270+hasanpeal@users.noreply.github.com>
2024-08-16 21:53:29 -0400

    Update DailyTemperature.java

--- DailyTemperature.java
+++ DailyTemperature.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
+        
         Stack <Integer> stack = new Stack<>();
         int[] res = new int[temperatures.length];
 
         for(int i = 0; i < temperatures.length; i++){
+            
             while(!stack.empty() && temperatures[i] > temperatures[stack.peek()]){
+                
                 int index = stack.pop();
                 res[index] = i - index;
             }
+            
             stack.push(i);
+            
         }
+        
         return res;
     }
 }
