226511efaf8c6cf528779619846952f7a8d8963b
Peal Hasan <95323270+hasanpeal@users.noreply.github.com>
2024-09-05 21:10:39 -0400

    Update IsAnagram.java

--- IsAnagram.java
+++ IsAnagram.java
@@ -9,7 +9,6 @@ class IsAnagram {
         for(int i = 0; i < s.length(); i++){
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
-
         for(int i = 0; i < t.length(); i++){
             map2.put(t.charAt(i), map2.getOrDefault(t.charAt(i), 0) + 1);
         }
