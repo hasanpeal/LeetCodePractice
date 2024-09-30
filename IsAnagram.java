a6b0b19e217b62bda40005c0af3d810b1abba1cf
Peal Hasan <95323270+hasanpeal@users.noreply.github.com>
2024-09-29 23:18:49 -0400

    Update IsAnagram.java

--- IsAnagram.java
+++ IsAnagram.java
@@ -1,6 +1,7 @@
 import java.util.HashMap;
 
 class IsAnagram {
+    
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
         HashMap <Character, Integer> map = new HashMap<>();
