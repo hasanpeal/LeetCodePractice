2bb81b0627b2d8661ed0e792cc2c29fb1ed13eed
Peal Hasan <95323270+hasanpeal@users.noreply.github.com>
2024-11-07 23:01:18 -0500

    Update ContainsDuplicate.java

--- ContainsDuplicate.java
+++ ContainsDuplicate.java
@@ -6,6 +6,7 @@ class Solution {
         HashMap <Integer, Integer> map = new HashMap<>();
         
         for(int i = 0; i < nums.length; i++){
+            \\
             
             if(map.containsKey(nums[i])) return true;
                 
